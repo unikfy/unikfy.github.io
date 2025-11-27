ba20e6dea4c5f8f58f1e74c41a73ca76955ab557
unikfy <tomseo.com@gmail.com>
2025-11-27 17:51:11 +0800

    Site updated: 2025-11-27 17:51:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/27/My-First-Post/","2025/11/27/hello-world/"];function toRandomPost(){
+var posts=["2025/11/27/这是一篇新的博文/","2025/11/27/My-First-Post/","2025/11/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
