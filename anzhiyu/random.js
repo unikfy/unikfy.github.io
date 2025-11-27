6975d99f767355f911c2016ab5501354788fc81b
unikfy <tomseo.com@gmail.com>
2025-11-27 17:24:40 +0800

    Site updated: 2025-11-27 17:24:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/27/hello-world/"];function toRandomPost(){
+var posts=["2025/11/27/My-First-Post/","2025/11/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
